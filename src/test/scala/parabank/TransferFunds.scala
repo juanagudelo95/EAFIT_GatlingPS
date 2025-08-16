d5cf54240e70d5bcfa738a211503b674746fc8fb
juanagudelo95 <juan.agudelo95@gmail.com>
2025-08-16 10:25:13 -0500

    Update TransferFunds.scala

--- src/test/scala/parabank/TransferFunds.scala
+++ src/test/scala/parabank/TransferFunds.scala
@@ -1,1 +1,18 @@
+package parabank
 
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import parabank.Data._
+
+class LoginTest extends Simulation{
+
+  // 1 Http Conf
+  val httpConf = http.baseUrl(url)
+    .acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+    .check(status.is(200))
+
+  // 2 transfer
+  val scn2 = scenario ("Transfer")
+    exec(http("transfer")
+        .post(s"/transfer/$fromAccountId/$toAccountId/$amount"))
