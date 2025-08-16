d779db882b5b41247c25b429a055f990062bb9e0
juanagudelo95 <juan.agudelo95@gmail.com>
2025-08-16 09:45:16 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,6 +4,10 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "juaneagudelo"
     val password = "testtest"
+    val fromAccountId = "17229" 
+    val toAccountId = "18784"
+    val ammount = "333333"
 
 }
+
 
